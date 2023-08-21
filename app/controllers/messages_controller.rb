f9ae59199f1293a6a66ee660fa90ab276e3dc118
Ilia Talebzadeh <ilyatalebzadeh82@gmail.com>
2023-08-21 15:02:28 +0330

    error fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,10 +14,12 @@ class MessagesController < ApplicationController
         pure_content = message_item&.strip
         return false if pure_content.nil?
 
-        last_message_hour = current_user.messages.last.created_at.hour
-        last_message_minutes = current_user.messages.last.created_at.min
+        last_message_hour = current_user.messages&.last&.created_at&.hour
+        last_message_minutes = current_user.messages&.last&.created_at&.min
         now_hours = Time.zone.now.hour
         now_minutes = Time.zone.now.min
+
+        return true if last_message_hour.nil? || last_message_minutes.nil?
 
         return false if (last_message_hour == now_hours) and (last_message_minutes == now_minutes)
 
